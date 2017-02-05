48db247fbab4f2c17342bdfd4e9204b23994bbc9
binaryfunt <binaryfunt@users.noreply.github.com>
2017-02-05 06:17:04 +0000

    Debugging news template (squashed)

--- breakfiller/breakfiller.js
+++ breakfiller/breakfiller.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
 
     var newsAPISrc = "https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=919866e8cdde468e954a8230d4c63ece",
-        newsTemplate = "news-template.html",
+        // newsTemplate = "news-template.html",
+        newsTemplate = "https://binaryfunt.github.io/breakfiller/news-template.html"
 
         mainDiv = $("#main > .wrapper")[0];
 
@@ -11,26 +12,29 @@ $(document).ready(function() {
         articles =  data.articles;
         for (var i = 0; i < articles.length; i++) {
             createArticle(articles[i]);
+            console.log(articles[i]);
         }
     }
 
-    function populateArticleTemplate(data) {
+    function populateArticleTemplate(template, article) {
         var content = {
-            title: data.title,
-            description: data.description
+            title: article.title,
+            description: article.description
         };
-        console.log(data);
-        // $.template("template", data);
+        console.log(template);
+        // $.template("template", template);
         // $.tmpl("template", article)
         //     .appendTo(mainDiv);
     }
 
     function createArticle(article) {
-        var content = {
-            title: article.title,
-            description: article.description
-        };
-        $.get(newsTemplate, populateArticleTemplate);
+        // var content = {
+        //     title: article.title,
+        //     description: article.description
+        // };
+        $.get(newsTemplate, function(data) {
+            populateArticleTemplate(data, article);
+        });
         // $.template("template", MEOW);
         // $.tmpl("template", article)
         //     .appendTo(mainDiv);
