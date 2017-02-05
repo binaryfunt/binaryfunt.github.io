805a9c690d9897bcecf4be61e606c6d3a445b1a6
binaryfunt <binaryfunt@users.noreply.github.com>
2017-02-05 06:12:10 +0000

    Add template for article creation

--- breakfiller/breakfiller.js
+++ breakfiller/breakfiller.js
@@ -1,3 +1,50 @@
 $(document).ready(function() {
-    
+
+    var newsAPISrc = "https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=919866e8cdde468e954a8230d4c63ece",
+        newsTemplate = "news-template.html",
+
+        mainDiv = $("#main > .wrapper")[0];
+
+
+
+    function handleNewsAPIReq(data) {
+        articles =  data.articles;
+        for (var i = 0; i < articles.length; i++) {
+            createArticle(articles[i]);
+        }
+    }
+
+    function populateArticleTemplate(data) {
+        var content = {
+            title: data.title,
+            description: data.description
+        };
+        console.log(data);
+        // $.template("template", data);
+        // $.tmpl("template", article)
+        //     .appendTo(mainDiv);
+    }
+
+    function createArticle(article) {
+        var content = {
+            title: article.title,
+            description: article.description
+        };
+        $.get(newsTemplate, populateArticleTemplate);
+        // $.template("template", MEOW);
+        // $.tmpl("template", article)
+        //     .appendTo(mainDiv);
+
+        // $("<div/>", {
+        //     text: article.title
+        // }).appendTo(mainDiv);
+    }
+
+    function getNews() {
+        $.get(newsAPISrc, handleNewsAPIReq, "json");
+    }
+
+
+    getNews();
+
 });
