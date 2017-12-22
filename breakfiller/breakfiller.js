52ff9fdaad3d9b5d7a294e823b3c1813d84c83d2
Ronnie Dutta <binaryfunt@users.noreply.github.com>
2017-12-22 22:21:02 +0000

    Create weather display enhancement functions

--- breakfiller/breakfiller.js
+++ breakfiller/breakfiller.js
@@ -220,10 +220,48 @@ $(document).ready(function() {
             return obj[key];
         });
     }
+    function erf(x){
+        // erf(x) = 2/sqrt(pi) * integrate(from=0, to=x, e^-(t^2) ) dt
+        // with using Taylor expansion,
+        //        = 2/sqrt(pi) * sigma(n=0 to +inf, ((-1)^n * x^(2n+1))/(n! * (2n+1)))
+        // calculationg n=0 to 50 below (note that inside sigma equals x when n = 0, and 50 may be enough)
+        var m = 1.00;
+        var s = 1.00;
+        var sum = x * 1.0;
+        for(var i = 1; i < 50; i++){
+            m *= i;
+            s *= -1;
+            sum += (s * Math.pow(x, 2.0 * i + 1.0)) / (m * (2.0 * i + 1.0));
+        }
+        return 2 * sum / Math.sqrt(3.14159265358979);
+    }
+
+    function kelToCel(kelvin) {
+        return Math.round(kelvin - 273.15);
+    }
+
+    function tempToHsl(temperature) {
+        var x = parseFloat(temp) + 100,
+    	   hue = 125 - 125*erf((x - 110) / 20);
+    	return "hsl("+hue+", 100%, 60%)";
+    }
 
     function truncate(text) {
         var truncRegex = new RegExp("^(.{" + descriptionTruncLen + "}[\\w']*).*");
         return text.replace(truncRegex, "$1").concat("\u2026");
+    }
+
+    function colorInTemperatures() {
+        $(".weather .temp").each(function() {
+            var temperature = this.dataset.temperature;
+            $(this).find("div").css("background", tempToHsl(temperature));
+        });
+    }
+    function rotateWeatherVanes() {
+        $(".weather .wind").each(function() {
+            var direction = this.dataset.direction;
+            $(this).find("img").css("transform", "rotate("+direction+"deg)");
+        });
     }
 
 
